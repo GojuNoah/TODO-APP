2001e5bbd0fda4e2cad4719ab4a5fe5d329cbfac
Noah Stone <gojunoah@gmail.com>
2025-09-28 23:04:35 +0000

    skeleton complete, need to finish MVP features

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const todoInput = document.getElementById('todo-input');
+const addBtn = document.getElementById('add-btn');
+const todoList = document.getElementById('todo-list');
 
+const taskListData = JSON.parse(localStorage.getItem("data")) || [];
+const currentTodo = todoInput.value();
+
+const removeSpecialChars = (val) => {
+  return val.trim().replace(/[^A-Za-z0-9\-\s]/g, '');
+}
+
+const addTodo = () => {
+    
+};
+
+addBtn.addEventListener('click', addTodo);
