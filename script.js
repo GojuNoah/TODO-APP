fcf0d254066ff5b978ffbfd8fbce5982f893a8c4
Noah Stone <gojunoah@gmail.com>
2025-10-02 19:25:05 +0000

    added functionality for adding todo with either the button or enter key

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
+// get info from html
 const todoInput = document.getElementById('todo-input');
 const addBtn = document.getElementById('add-btn');
 const todoList = document.getElementById('todo-list');
 
-const taskListData = JSON.parse(localStorage.getItem("data")) || [];
-const currentTodo = todoInput.value();
+// get data from local storage
+const todoData = JSON.parse(localStorage.getItem("data")) || [];
 
-const removeSpecialChars = (val) => {
-  return val.trim().replace(/[^A-Za-z0-9\-\s]/g, '');
+// load previously added todos
+const renderTodos = () => {
+  todoList.innerHTML = "";
+  for (let i = 0; i < todoData.length; i++) {
+    todoList.insertAdjacentHTML("afterbegin", 
+      `<div class=todo-container>
+           <p class=todo-txt>${todoData[i]}</p>
+       </div>`);
+  }
 }
 
+// function to add new todo from input
 const addTodo = () => {
-    
+  const currentTodo = todoInput.value;
+  todoData.push(currentTodo);
+  localStorage.setItem("data", JSON.stringify(todoData));
+  todoInput.value = "";
+  renderTodos();
 };
 
+// call the loading of todos
+renderTodos();
+// event listener to handle add todo action
 addBtn.addEventListener('click', addTodo);
