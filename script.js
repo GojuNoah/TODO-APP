0defb413177181243f4c1fb10b1e8b366faf1316
Noah Stone <gojunoah@gmail.com>
2025-10-02 22:22:58 +0000

    added edit btn with functionality and added validation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // get info from html
 const todoInput = document.getElementById('todo-input');
-const addBtn = document.getElementById('add-btn');
+const addBtn = document.getElementById('initial-btn');
 const todoList = document.getElementById('todo-list');
 
 // get data from local storage
@@ -12,18 +12,24 @@ const renderTodos = () => {
   for (let i = 0; i < todoData.length; i++) {
     todoList.insertAdjacentHTML("afterbegin", 
       `<div class=todo-container id=${i}>
-           <p class=todo-txt>${todoData[i]}</p><button onclick=deleteTodo(this) class=del-btn>Delete Todo</button>
+           <p class=todo-txt>${todoData[i]}</p>
+           <button onclick=deleteTodo(this) class=del-btn>Delete Todo</button>
+           <button onclick=editTodo(this) class=edit-btn id=edit-btn>Edit Todo</button>
        </div>`);
-  }
-}
+  };
+};
 
 // function to add new todo from input
 const addTodo = () => {
   const currentTodo = todoInput.value.trim();
-  todoData.push(currentTodo);
-  localStorage.setItem("data", JSON.stringify(todoData));
-  todoInput.value = "";
-  renderTodos();
+  if (currentTodo === "") {
+    alert("Please enter a todo");
+  } else {
+    todoData.push(currentTodo);
+    localStorage.setItem("data", JSON.stringify(todoData));
+    todoInput.value = "";
+    renderTodos();
+  };
 };
 
 // function to delete todo from list
@@ -33,6 +39,21 @@ const deleteTodo = button => {
   todoData.splice(thisTodo, 1);
   localStorage.setItem("data", JSON.stringify(todoData));
   renderTodos();
+};
+
+// function to edit selected todo from list
+const editTodo = button => {
+  thisTodo = button.parentElement.id;
+  thisTodoInt = parseInt(thisTodo);
+  thisTodo = todoData[thisTodoInt];
+  const newText = prompt("Edit your todo:", thisTodo);
+  if (newText === "") {
+    alert("Please enter new text.");
+  } else if (newText !== null && newText.trim() !== "") {
+    todoData[thisTodoInt] = newText;
+    localStorage.setItem("data", JSON.stringify(todoData));
+    renderTodos();
+  };
 };
 
 // call the loading of todos
