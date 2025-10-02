897588ee04e6dd9c7f128071926bcb50659d10e1
Noah Stone <gojunoah@gmail.com>
2025-10-02 20:01:12 +0000

    added functional  delete btn to todo items

--- script.js
+++ script.js
@@ -11,22 +11,37 @@ const renderTodos = () => {
   todoList.innerHTML = "";
   for (let i = 0; i < todoData.length; i++) {
     todoList.insertAdjacentHTML("afterbegin", 
-      `<div class=todo-container>
-           <p class=todo-txt>${todoData[i]}</p>
+      `<div class=todo-container id=${i}>
+           <p class=todo-txt>${todoData[i]}</p><button onclick=deleteTodo(this) class=del-btn>Delete Todo</button>
        </div>`);
   }
 }
 
 // function to add new todo from input
 const addTodo = () => {
-  const currentTodo = todoInput.value;
+  const currentTodo = todoInput.value.trim();
   todoData.push(currentTodo);
   localStorage.setItem("data", JSON.stringify(todoData));
   todoInput.value = "";
   renderTodos();
 };
 
+// function to delete todo from list
+const deleteTodo = button => {
+  thisTodo = button.parentElement.id;
+  thisTodo = parseInt(thisTodo);
+  todoData.splice(thisTodo, 1);
+  localStorage.setItem("data", JSON.stringify(todoData));
+  renderTodos();
+};
+
 // call the loading of todos
 renderTodos();
-// event listener to handle add todo action
-addBtn.addEventListener('click', addTodo);
+
+// event listeners to handle add todo action
+addBtn.addEventListener('click', addTodo());
+todoInput.addEventListener('keydown', (event) => {
+  if (event.key === "Enter") {
+    addTodo();
+  }
+});
